696f1aba3035cf9d7b9ea511d154fe4ee71a21e8
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-09-29 16:10:04 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 export default function App() {
   return (
     <>
       <main className="bg-orange-100 flex flex-col items-center min-h-screen">
         <Header />
+        <Search />
       </main>
     </>
   );
