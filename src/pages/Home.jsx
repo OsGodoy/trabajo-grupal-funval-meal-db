8dbe2d6ec35b8f5be67b781285acfb1644bbc926
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-01 15:16:04 -0500

    cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import Header from "../components/Header";
+import Search from "../components/Search";
+
 function Home() {
-    return (
-        <>
-        </>
-    );
+  return (
+    <>
+      <main className="bg-orange-100 flex flex-col items-center min-h-screen">
+        <Header/>
+        <Search />
+      </main>
+    </>
+  );
 }
 
 export default Home;
