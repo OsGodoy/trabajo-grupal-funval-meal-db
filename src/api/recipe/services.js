3a3c72caac34382f72c51d53215429116a199e87
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-01 12:28:14 -0500

    cambios

--- src/api/recipe/services.js
+++ src/api/recipe/services.js
@@ -1,19 +1,20 @@
 import client from "./client";
 
-
 const getRandomMeals = async (amount = 10) => {
   const requests = Array.from({ length: amount }, () =>
     client.get("random.php")
   );
 
   const responses = await Promise.all(requests);
-  return responses.map(r => r.data.meals[0]);
+  return responses.map((r) => r.data.meals[0]);
 };
 
-const searchMeals = (query="Arrabiata") =>  client.get("/search.php", { params: {  s:query } });
+const searchMeals = (query = "Arrabiata") =>
+  client.get("/search.php", { params: { s: query } });
 
-const searchMealDetails = (id) =>  client.get("/lookup.php", { params: {  i:id } });
+const searchMealDetails = (id) =>
+  client.get("/lookup.php", { params: { i: id } });
 
-const getAreas = (id) =>  client.get("/list.php", { params: {  a:list } });
+const getAreas = (id) => client.get("/list.php", { params: { a: list } });
 
 export { getRandomMeals, searchMeals, searchMealDetails, getAreas };
