1f5d42f6d55da42df4b912867556fa8a7062610d
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-01 15:57:58 -0500

    cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
+import LoadingCarousel from "../components/LoadingCarousel";
 import Search from "../components/Search";
 
 function Home() {
   return (
     <>
       <main className="bg-orange-100 flex flex-col items-center min-h-screen">
-        <Header/>
+        <Header />
         <Search />
+        <LoadingCarousel />
       </main>
     </>
   );
