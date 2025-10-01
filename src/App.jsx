f6dd39216bcbb79e6d4076152ed281854dc88c5a
Luis Palacio <palaciodimasluisenrique@gmail.com>
2025-10-01 14:59:59 -0500

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@
 import React from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MealRecipes from "./pages/MealRecipes";
+import MealRecipeDetails from "./pages/MealRecipeDetails";
 
 
 export default function App() {
   return (
     <>
-      <main className="bg-orange-100 flex flex-col items-center min-h-screen">
-        <Header />
-        <Search />
-      </main>
+      <Routes>
+        <Route path="/" elemen={<Home />} ></Route>
+        <Route path="/meal-recipes" elemen={<MealRecipes />} ></Route>
+        <Route path="/Meal-recipe-details/:id" elemen={<MealRecipeDetails />} ></Route>
+      </Routes>
     </>
   );
 }
